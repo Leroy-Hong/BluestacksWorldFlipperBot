c9f138a7fe1ceb8335fbc5ec39307e9a377322b4
Leroy-Hong <69704784+Leroy-Hong@users.noreply.github.com>
2022-06-23 21:20:58 +0800

    removed Test files

--- Test/TempScreenCap.py
+++
@@ -1,4 +0,0 @@
-from adbutils import adb
-d = adb.device()
-screenFeed = d.shell(["screencap", "-p", ">", "/sdcard/screenCapTest.png"])
-d.sync.pull("/sdcard/screenCapTest.png", "D:/Documents/PythonProjects/BluestacksWorldFlipper/screenCapTest.png")
